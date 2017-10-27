2acfb1596cb132fabaf9e9a7dd4a5864a5865fa6
ChenWangCarleton <wch4university@gmail.com>
2017-10-26 20:18:19 -0400

    getall & searchbyid finished
    server side needs userSearch.java
    client side needs clientSearch.java
    
    both side need itemForGetAll.java
    and latest version of mainItem.java
    and need all the jars.
    in order to test the server side, needs the mainJson.json
    in order to text the client side correctly, please only use the exact string read from the server side's output file
    or just the string with correct format

--- code/UserSearch.java
+++ code/UserSearch.java
@@ -6,28 +6,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonValue;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 public class UserSearch {
 	String[] categories= {"Fruits & Vegetables","Deli & Ready Meals","Bakery","Meat & Seafood","Dairy and Eggs","Drinks","Frozen","Pantry"};
 	String source="D:\\MainJson.json";
-	public void generalSearch(String target) throws IOException {
+	String[] storesOrder= {"Loblaws","Independent","Walmart"};
+	public String generalSearch(String target) throws IOException {
 		File f=new File(target);
 		PrintWriter fw=new PrintWriter(new FileWriter(f));
 		ObjectMapper mapper = new ObjectMapper();
 		File jsonInput=new File(source);
 		InputStream is;
-		ArrayList<mainItem>  Itemss=new ArrayList<>();
+		ArrayList<itemForGetAll>  Items=new ArrayList<>();
 		is=new FileInputStream(jsonInput);
 		JsonReader reader=Json.createReader(is);
 		JsonObject itObj=reader.readObject();
@@ -35,15 +31,28 @@ public class UserSearch {
 		for(int x=0;x<categories.length;x++) {
 			 JsonArray itsObj=itObj.getJsonArray(categories[x]);
 			 for(JsonValue value: itsObj) {
-				 Itemss.add(mapper.readValue(value.toString(), mainItem.class));
+				 mainItem e=mapper.readValue(value.toString(), mainItem.class);
+				 Items.add(new itemForGetAll(e.productID,e.name,e.category));
 			 }
+
 		}
+		String result="[";
+		 for(int y=0;y<Items.size();y++) {
+			 if(y==Items.size()-1) {
+				 result=result+"{\"id\":"+Items.get(y).id+",\"na\":\""+Items.get(y).na+"\",\"ca\":\""+Items.get(y).ca+"\"}";
+			 }
+			 else {
+				 result=result+"{\"id\":"+Items.get(y).id+",\"na\":\""+Items.get(y).na+"\",\"ca\":\""+Items.get(y).ca+"\"},";
+			 }
+		 }
+		 result=result+"]";
+		 fw.print(result);
+		 fw.flush();
+		 fw.close();
+		 return target;//return the path . if succeed. please check with the input path.
 	}
-	public ArrayList<mainItem> searchByName(String name){
-		ArrayList<mainItem> result=new ArrayList<>();
-		return result;
-	}
-	public mainItem searchByID(int id) throws IOException {
+
+	public String searchByID(int id,String target) throws IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		File jsonInput=new File(source);
 		InputStream is;
@@ -60,7 +69,50 @@ public class UserSearch {
 			 }
 			 for(int o=0;o<Itemss.size();o++) {
 				 if(id==Itemss.get(o).productID) {
-					 return Itemss.get(o);
+						String price="[";
+						String store="[";
+						for(int i=0;i<storesOrder.length;i++) {
+							if(Itemss.get(o).stores[i]!=null) {
+								if(i!=storesOrder.length-1) {
+									if(Itemss.get(o).price[i]!=null) {
+										price=price+"\""+Itemss.get(o).price[i]+"\",";
+									}
+									else {
+										price=price+"null,";
+									}
+									store=store+"\""+Itemss.get(o).stores[i]+"\",";
+								}
+								else {
+									if(Itemss.get(o).price[i]!=null) {
+										price=price+"\""+Itemss.get(o).price[i]+"\"";
+									}
+									else {
+										price=price+"null";
+									}
+									store=store+"\""+Itemss.get(o).stores[i]+"\"";							
+								}
+							}
+							else {
+								if(i!=storesOrder.length-1) {
+									price=price+"null,";
+									store=store+"null,";
+								}
+								else {
+									price=price+"null";
+									store=store+"null";							
+								}						
+							}
+						}
+						price+="]";
+						store+="]";
+						String description=Itemss.get(o).description==null?"null":"\""+Itemss.get(o).description+"\"";
+					 String result="{\"productID\":"+Itemss.get(o).productID+",\"category\":\""+Itemss.get(o).category+"\",\"name\":\""+Itemss.get(o).name+"\",\"stores\":"+store+",\"price\":"+price+",\"description\":"+description+"}";
+					 File tar=new File(target);
+					 PrintWriter fw=new PrintWriter(new FileWriter(tar));
+					 fw.print(result);
+					 fw.flush();
+					 fw.close();
+					 return target;
 				 }
 			 }
 		}
@@ -68,19 +120,18 @@ public class UserSearch {
 	}
 	public static void main(String[] args) {
 		UserSearch us=new UserSearch();
-		try {
+	/*	try {//getall
 			us.generalSearch( "D:\\GeneralSearch.json");
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
-		
-		try {
-			System.out.println(us.searchByID(3242).toString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		}*/
+		try {//searchbyid
+			us.searchByID(49,	"D:\\ByID.json");
+		}catch(Exception e) {
 			e.printStackTrace();
 		}
+
 
 	}
 
---
+++ code/clientSearch.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.type.TypeFactory;
+
+public class clientSearch{
+	public ArrayList<itemForGetAll> readGetAll(String jsonStr) throws  Exception{
+	//	ArrayList<itemForGetAll> result=new ArrayList<>();
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayList<itemForGetAll> result = mapper.readValue(jsonStr, TypeFactory.defaultInstance().constructCollectionType(ArrayList.class, itemForGetAll.class));
+		return result;
+	}
+	public mainItem readSearchID(String jsonStr) throws Exception{
+		ObjectMapper mapper = new ObjectMapper();
+		mainItem result=mapper.readValue(jsonStr, mainItem.class);
+		return result;
+	}
+	public static void main(String[] args) throws Exception {
+		clientSearch cs=new clientSearch();
+		/*String id="D:\\ByID.json";//test for read searchbyid string
+		File i=new File(id);
+ 		String text  = "";
+		try{
+	 		Scanner fileScanner = new Scanner(i);
+	 		while (fileScanner.hasNextLine()) {
+	 		    // get the token *once* and assign it to a local variable
+	 		    text += fileScanner.nextLine();
+	 		}
+	 			fileScanner.close();
+	 		//	System.out.println(text);
+	 	}
+		catch(IOException e){
+	 			System.out.println("scanner could not open");
+	 	}
+		mainItem s=cs.readSearchID(text);
+		System.out.println(s.toString());*/
+		
+		String all="D:\\GeneralSearch.json";//test for read getall string
+		File i=new File(all);
+		String text="";
+		try{
+	 		Scanner fileScanner = new Scanner(i);
+	 		while (fileScanner.hasNextLine()) {
+	 		    // get the token *once* and assign it to a local variable
+	 		    text += fileScanner.nextLine();
+	 		}
+	 			fileScanner.close();
+	 		//	System.out.println(text);
+	 	}
+		catch(IOException e){
+	 			System.out.println("scanner could not open");
+	 	}
+	//	System.out.println(text);
+		ArrayList<itemForGetAll> result=cs.readGetAll(text);
+		for(int x=0;x<result.size();x++) {
+			System.out.println(result.get(x).toString());
+		}
+	}
+}
