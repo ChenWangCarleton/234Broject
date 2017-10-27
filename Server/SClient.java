f77b0d38c62574e515c5b2418f3c71431a93dd32
Abdullrhman Aljasser <abdullrhmanaljasser@gmail.com>
2017-10-26 22:18:59 -0400

    Server

---
+++ Server/SClient.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+
+public class SClient {
+	private DatagramSocket clientSocket;
+	private InetAddress IPAddress;
+	private byte[] sendData;
+	private byte[] receiveData;
+	
+	public SClient() throws Exception {
+		this.clientSocket = new DatagramSocket(); //Client socket (Only 1 needed)
+		this.IPAddress = InetAddress.getByName("99.236.248.228"); //Server IP Address
+		this.sendData = new byte[1024]; //Amount of data we send to server
+		this.receiveData = new byte[1024]; //Amount expect to receive
+	}
+	
+	public void sendReq(String reqType) throws Exception {
+		sendData = reqType.getBytes(); //Return the request type (reqType)
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 60700); //Establishing request message (With IP and port)
+		clientSocket.send(sendPacket); //Send request
+		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); //Create packet for received message
+		clientSocket.receive(receivePacket); //Wait and ready to receive
+		String modifiedSentence = new String(receivePacket.getData()); //Turn data into STRING
+		System.out.println("FROM SERVER:" + modifiedSentence); //Print string
+		clientSocket.close(); //Close socket.
+	}
+	public static void main(String args[]) throws Exception {
+		SClient client = new SClient();
+		try{
+			client.sendReq("Hello");
+		} catch (IOException e) {
+			System.err.println("Caught IOException: " + e.getMessage());
+		}
+	}
+}
---
+++ Server/SServer.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+
+public class SServer {
+	private static DatagramSocket serverSocket;
+	private byte[] receiveData;
+	private byte[] sendData;
+	private SServer() throws Exception {
+		this.serverSocket = new DatagramSocket(60700);
+		this.receiveData = new byte[1024];
+		this.sendData = new byte[1024];
+	}
+	public void listen() throws Exception {
+		while(true){
+			// *** Retrevial of data ***
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			serverSocket.receive(receivePacket); //Received data from client.
+			String sentence = new String(receivePacket.getData()); //Retreving the data.
+			System.out.println("Received: " + sentence);
+			
+			
+			// *** The start of sending back ***
+			InetAddress IPAddress = receivePacket.getAddress(); // Client IPAdress.
+			int port = receivePacket.getPort(); //Getting client port.
+			String capitalizedSentence = sentence.toUpperCase(); //Sending back msg upper case
+            sendData = capitalizedSentence.getBytes(); //Turn data into byte.
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port); //Creating packet to send data
+            serverSocket.send(sendPacket); //Sending
+		}
+	}
+	public static void main(String args[]) throws Exception {
+		SServer server = new SServer();
+		try{
+			server.listen();
+		} catch (IOException e) {
+			System.err.println("Caught IOException: " + e.getMessage());
+		}
+	}
+}
