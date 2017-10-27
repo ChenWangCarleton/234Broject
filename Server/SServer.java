7bd48ff26ec53a9629d6790e99592bfa20542e67
Abdullrhman Aljasser <abdullrhmanaljasser@gmail.com>
2017-10-26 22:43:08 -0400

    Server

--- Server/SServer.java
+++ Server/SServer.java
@@ -12,23 +12,54 @@ public class SServer {
 	}
 	public void listen() throws Exception {
 		while(true){
+			System.out.println("Success!... Waiting for a client...\n");
 			// *** Retrevial of data ***
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket); //Received data from client.
 			String sentence = new String(receivePacket.getData()); //Retreving the data.
-			System.out.println("Received: " + sentence);
+			System.out.println("Got client!");
+			
+			switch(sentence){
+				case "GetAll":
+					InetAddress IPAddress = receivePacket.getAddress(); // Client IPAdress.
+					int port = receivePacket.getPort(); //Getting client port.
+					
+					//Get data
+					sendData = ; //Data we sending.
+					//Get data
+					
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port); //Creating packet to send data
+					serverSocket.send(sendPacket); //Sending
+					break;
+				default:
+					InetAddress IPAddress = receivePacket.getAddress(); // Client IPAdress.
+					int port = receivePacket.getPort(); //Getting client port.
+					
+					//Get data
+					sendData = ; //Data we sending.
+					//Get data
+					
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port); //Creating packet to send data
+					serverSocket.send(sendPacket); //Sending
+					break;
+				
+			}
 			
 			
-			// *** The start of sending back ***
+			/*
 			InetAddress IPAddress = receivePacket.getAddress(); // Client IPAdress.
 			int port = receivePacket.getPort(); //Getting client port.
 			String capitalizedSentence = sentence.toUpperCase(); //Sending back msg upper case
             sendData = capitalizedSentence.getBytes(); //Turn data into byte.
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port); //Creating packet to send data
-            serverSocket.send(sendPacket); //Sending
+            serverSocket.send(sendPacket); //Sending*/
 		}
 	}
 	public static void main(String args[]) throws Exception {
+		
+		//Running server
+		System.out.println("Starting server...\n");
+		
 		SServer server = new SServer();
 		try{
 			server.listen();
