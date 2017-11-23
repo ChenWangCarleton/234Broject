e8e07d0dae0109af04a73c6b539f5fd7824196b1
ChenWangCarleton <wch4university@gmail.com>
2017-11-23 16:18:14 -0500

    this is a refined version of WithBrand code.

---
+++ refined version of code/Independent.java
@@ -0,0 +1,266 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+public class Independent{
+	File target;
+	String blank="     ";
+//	Document document;
+	TreeSet<String> firstLevel=new TreeSet<>();
+	TreeSet<String> secondLevel=new TreeSet<>();
+	TreeSet<String> thirdLevel=new TreeSet<>();
+	PrintWriter pw;
+	String[] categories= {"Fruits & Vegetables","Deli & Ready Meals","Bakery","Meat & Seafood","Dairy and Eggs","Drinks","Frozen","Pantry"};
+	String url="https://www.yourindependentgrocer.ca";
+	static boolean status=false;
+	ArrayList<String> failUrl=new ArrayList<>();
+	public Independent() {
+		status=false;
+	}
+	public void execute(String targetFile) throws Exception {
+		status=false;
+		target=new File(targetFile);
+		pw=new PrintWriter(new FileWriter(target));
+		String first="/Independent/Food";
+		String source= "";
+		FirstLevel(source,first);
+		status=true;
+	}
+	public void FirstLevel(String source,String first) throws Exception {
+		Document doc=Jsoup.connect(url+source).get();
+		Elements e=doc.select("li[data-level=1]");
+		ArrayList<String> firstl=new ArrayList<>();;
+		ArrayList<String> text=new ArrayList<>();
+		for(int x=0;x<e.size();x++) {
+			Element a=e.get(x).select("a").first();
+			if(a.attr("href").startsWith(first)) {
+				if(firstLevel.add(a.attr("href"))) {
+					firstl.add(a.attr("href"));
+					text.add(a.text());
+				}
+			}
+		}
+		int fru=0;
+		int org=8;
+		boolean firstS=true;
+		for(int x=0;x<firstl.size();x++) {
+			String fl=firstl.get(x);
+			if(fl.contains("Fruits-%26-Vegetables")) {
+				fru=x;
+				continue;
+			}
+			if(fl.contains("Natural-%26-Organic")) {
+				org=x;
+				continue;
+			}
+			if(firstS) {
+				pw.println("{\""+text.get(x)+"\":[");
+			}
+			else {
+				pw.println("],");
+				pw.println("\""+text.get(x)+"\":[");
+			}
+			pw.flush();
+			System.out.println(firstl.get(x)+"  --->  "+url+fl);
+			String second=fl.substring(0,fl.indexOf("/c/"));
+			SecondLevel(firstl.get(x),second,text.get(x));
+			firstS=false;
+		}
+		String fl=firstl.get(fru);
+		System.out.println(firstl.get(fru)+"  --->  "+url+fl);
+		String second=fl.substring(0,fl.indexOf("/c/"));
+		pw.println("],");
+		pw.println("\""+text.get(fru)+"\":[");
+		pw.flush();
+		SecondLevel(firstl.get(fru),second,text.get(fru));
+		
+//		fl=firstl.get(org);
+//		second=fl.substring(0,fl.indexOf("/c/"));
+//		SecondLevel(firstl.get(org),second,text.get(fru));
+		pw.println("]}");
+		pw.flush();
+
+		
+	}
+	public void SecondLevel(String source, String second,String cate) throws Exception {
+		Document doc=Jsoup.connect(url+source).get();
+		ArrayList<String> sle=new ArrayList<>();
+		ArrayList<String> text=new ArrayList<>();
+		Elements e=doc.select("li[data-level=2]");
+		for(int x=0;x<e.size();x++) {
+			Element a=e.get(x).select("a").first();
+			if(a.attr("href").startsWith(second)&&!a.attr("href").contains(source)) {
+				if(secondLevel.add(a.attr("href"))){
+					sle.add(a.attr("href"));
+					text.add(a.text());
+				}
+			}
+		}
+		for(int x=0;x<sle.size();x++) {
+			String sl=sle.get(x);
+			System.out.println(blank+sle.get(x)+"  --->  "+url+sl);
+			String third=sl.substring(0,sl.indexOf("/c/"));
+			if(x==sle.size()-1) {
+				ThirdLevel(sle.get(x),third,true,cate);
+			}
+			else {
+				ThirdLevel(sle.get(x),third,false,cate);
+			}
+		}
+	}
+	public void ThirdLevel(String source, String third, boolean isLast,String cate) throws Exception {
+		Document doc=Jsoup.connect(url+source).get();
+		boolean hasPro=true;
+		Elements e=doc.select("li[data-level=3]");
+		ArrayList<String> tle=new ArrayList<>();
+		ArrayList<String> text=new ArrayList<>();
+		for(int x=0;x<e.size();x++) {			
+			Element a=e.get(x).select("a").first();
+			if(a.attr("href").startsWith(third)&&!a.attr("href").contains(source)) {
+				if(thirdLevel.add(a.attr("href"))){
+					hasPro=false;
+					tle.add(a.attr("href"));
+					text.add(a.text());
+				}
+			}
+			
+		}
+		if(hasPro) {
+			if(isLast) {
+				productLevel(source,true,cate);
+			}
+			else {
+				productLevel(source,false,cate);
+			}
+		}
+		else {
+			for(int x=0;x<tle.size();x++) {
+				String tl=tle.get(x);
+				System.out.println(blank+blank+text.get(x)+"  --->  "+url+tl);
+				if(isLast&&x==tle.size()-1) {
+					productLevel(tl,true,cate);
+				}
+				else {
+					productLevel(tl,false,cate);
+				}
+			}
+		}
+	}
+	private static String[][] UMLAUT_REPLACEMENTS = { { new String("Ä"), "A" }, { new String("Ü"), "U" }, { new String("Ö"), "O" }, { new String("ä"), "a" }, { new String("ü"), "u" }, { new String("ö"), "o" }, { new String("ß"), "ss" },{new String("é"),"e"} ,{new String("É"),"E"}};
+	public static String replaceUmlaute(String orig) {
+	    String result = orig;
+
+	    for (int i = 0; i < UMLAUT_REPLACEMENTS.length; i++) {
+	        result = result.replace(UMLAUT_REPLACEMENTS[i][0], UMLAUT_REPLACEMENTS[i][1]);
+	    }
+
+	    return result;
+	}
+	ArrayList<String> brands=new ArrayList<>();//////
+
+	public void addToBrands(String brand) {
+		if(brand.length()==0||brand.equals("")||brand==null)return;
+		//HashSet<String> notBrand=new HashSet<>();
+	//	notBrand.add("Coca-Cola");
+	//	notBrand.add("Pepsi");
+	//	notBrand.add("Dr. Pepper");
+	//	notBrand.add("Sprite");
+		brand=replaceUmlaute(brand);
+		if(brand.length()>0) {
+			if(brand.charAt(brand.length()-1)==' ')brand=brand.substring(0, brand.length()-1);
+		}
+	//	if(brand.equals("p"))brand="Dare";
+	//	if(notBrand.contains(brand))
+		//	return;
+		for(int x=0;x<brands.size();x++) {
+			if(brands.get(x).equals(brand))
+				return;
+		}
+		brands.add(brand);
+	}
+	public void productLevel(String source,boolean isLast,String cate) throws Exception {
+		Document doc=Jsoup.connect(url+source).get();
+		ArrayList<String> n=new ArrayList<>();
+		ArrayList<String> d=new ArrayList<>();
+		ArrayList<String> pr=new ArrayList<>();
+		ArrayList<String> b=new ArrayList<>();
+		
+		Elements e=doc.select("div.product-info");
+		System.out.println("raw product size :       "+e.size());
+		for(int x=0;x<e.size();x++) {
+			Element p=e.get(x);
+		//	Element name=p.select
+			String brand=p.select("span.js-product-entry-brand").text();
+			brand=brand.replaceAll("\u00a0"," ");
+			String name=p.select("span.js-product-entry-name").text();
+			String price=p.select("span.reg-price-text").text();
+			String description=p.select("span.js-product-entry-size-detail").text();
+			if(brand==null||brand.equals("")||brand.length()==0) {
+				brand="null";
+			}
+			else {
+				brand=replaceUmlaute(brand);
+				if(brand.charAt(brand.length()-1)==' ')brand=brand.substring(0, brand.length()-1);
+				brands.add(brand);
+				brand="\""+brand.replaceAll("\"", "'")+"\"";
+			}
+			name=replaceUmlaute(name);
+			name="\""+name.replaceAll("\"", "'")+"\"";
+			if(price==null||price.equals("")||price.length()==0) {
+				price="null";
+			}
+			else {
+				price="\""+price+"\"";
+			}
+			if(description==null||description.equals("")||description.length()==0) {
+				description="null";
+			}
+			else {
+				description="\""+description.replaceAll("\"", "'")+"\"";
+			}
+			description=description.replaceAll("\\(", "");
+			description=description.replaceAll("\\)", "");
+			n.add(name);
+			pr.add(price);
+			d.add(description);
+			b.add(brand);
+
+		}
+		
+
+		//System.out.println("product sizee: "+products.size()+blank+blank+doc.title()+"     product Level:");
+
+		for(int x=0;x<n.size();x++) {
+			if(isLast&&x==n.size()-1) {
+				pw.println("{\"category\":\""+cate+"\",\"name\":"+n.get(x)+",\"description\":"+d.get(x)+",\"brand\":"+b.get(x)+",\"price\":"+pr.get(x)+",\"store\":\"Independent\"}");
+			}
+			else {
+				pw.println("{\"category\":\""+cate+"\",\"name\":"+n.get(x)+",\"description\":"+d.get(x)+",\"brand\":"+b.get(x)+",\"price\":"+pr.get(x)+",\"store\":\"Independent\"},");
+			}
+			pw.flush();
+			System.out.println(n.get(x)+"   "+d.get(x)+"    "+pr.get(x)+"   "+b.get(x));	
+		//	System.out.println(blank+blank+blank+products.keySet().toArray()[x]+"    price:  "+products.get(products.keySet().toArray()[x]));
+		}
+	}
+	public static void main(String args[]) throws IOException{
+		String fn="D:\\IndependentWithBrand.json";
+		Independent l=new Independent();
+		try {
+			l.execute(fn);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			System.out.println(l.status);
+		}
+	}
+}
---
+++ refined version of code/Walmart.java
@@ -0,0 +1,374 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+public class Walmart{
+	File target;
+	PrintWriter fw;
+	String[] categories= {"Fruits & Vegetables","Deli & Ready Meals","Bakery","Meat & Seafood","Dairy and Eggs","Drinks","Frozen","Pantry"};
+	String[] links= {"/fruits-and-vegetables/N-3799","/deli/N-3792","/bakery/N-3796","/meat-seafood/N-3793","/dairy-eggs/N-3798","/drinks/N-3791","/frozen-food/N-3795","/pantry-food/N-3794"};
+	String[] linksBrand= {"/bakery/bagels-buns-rolls/N-3954","/bakery/desserts/N-3846","/bakery/sliced-bread/N-3845","/bakery/wraps-pita-flatbread/N-3955","/dairy-eggs/eggs/N-3850","/dairy-eggs/milk-cream/N-3851","/dairy-eggs/cheese/N-3849","/dairy-eggs/yogurt/N-3853","/deli/hummus-spreads/N-3953","/deli/deli-cheese/N-3817","/deli/deli-meat/N-3818","/grocery/produce/tofu-soy-products/N-4523","/drinks/coffee/N-3815","/drinks/tea-hot-drinks/N-3812","/drinks/water/N-3813","/drinks/juice/N-3810","/frozen-food/ice-cream-treats/N-3828","/frozen-food/meals-sides/N-3826","/frozen-food/pizza/N-3832","/frozen-food/vegetables/N-3825","/fruits/N-3852","/vegetables/N-3854","/salad-greens-herbs/N-4524","/meat-seafood/seafood/N-3824","/meat-seafood/beef/N-3820","/meat-seafood/pork/N-3822","/meat-seafood/poultry/N-3821","/pantry-food/pasta-rice-beans/N-3835","/pantry-food/baking-needs/N-3833","/pantry-food/canned-food/N-3839","/pantry-food/chips-snacks/N-3842","/natural-organic-food/N-3992"};
+	
+	String url="https://www.walmart.ca/en/grocery";
+	WebDriver ABdriver;
+	WebDriver tempDri;
+	WebDriver dri;
+	WebDriver driver;
+	
+	static boolean  status=false;
+	ArrayList<String> brands=new ArrayList<>();//////
+	ArrayList<String> failUrl=new ArrayList<>();
+	private static String[][] UMLAUT_REPLACEMENTS = { { new String("Ä"), "A" }, { new String("Ü"), "U" }, { new String("Ö"), "O" }, { new String("ä"), "a" }, { new String("ü"), "u" }, { new String("ö"), "o" }, { new String("ß"), "ss" },{new String("é"),"e"} ,{new String("É"),"E"}};
+	public static String replaceUmlaute(String orig) {
+	    String result = orig;
+
+	    for (int i = 0; i < UMLAUT_REPLACEMENTS.length; i++) {
+	        result = result.replace(UMLAUT_REPLACEMENTS[i][0], UMLAUT_REPLACEMENTS[i][1]);
+	    }
+
+	    return result;
+	}
+	public boolean hasMoreBrand(WebElement w) {
+		try {
+			WebElement m=w.findElement(By.className("moreLess"));
+	//		System.out.println("text:    "+m.getText()+"       link:"+m.getAttribute("link"));
+			if(m.getAttribute("link").contains("showMoreIds")) {
+			//	m.click();
+				return true;
+			}
+			else return false;
+		}catch(Exception e) {
+				e.printStackTrace();		
+				return false;
+		}
+	}
+	public String getBrand(ArrayList<String> brands,String name) {// set brand as an attribute.  read and store all the brand first.!!!
+		for(int x=0;x<brands.size();x++) {
+			if(name.contains(brands.get(x))||name.contains(brands.get(x).toLowerCase())||name.contains(brands.get(x).toUpperCase())) {//startsWith or Contains
+				return brands.get(x);
+			}
+		}
+		
+		return null;
+	}
+	public String removeBrand(String name, String brand) {
+		if(name.contains(brand)) {
+			name=name.replace(brand, "");
+		}
+		else if(name.contains(brand.toLowerCase())){
+			name=name.replace(brand.toLowerCase(), "");
+
+		}
+		else if(name.contains(brand.toUpperCase())) {
+			name=name.replace(brand.toUpperCase(), "");
+		}
+		if(name.startsWith(" ")) {
+			name=name.substring(1);
+		}
+		while(name.charAt(name.length()-1)==' '||name.charAt(name.length()-1)==','||name.charAt(name.length()-1)=='.') {
+			name=name.substring(0, name.length()-1);
+		}
+		return name;
+	}
+	public void addToBrands(String brand) {
+		HashSet<String> notBrand=new HashSet<>();
+		notBrand.add("Coca-Cola");
+		notBrand.add("Pepsi");
+		notBrand.add("Dr. Pepper");
+		notBrand.add("Sprite");
+		brand=replaceUmlaute(brand);
+		if(brand.equals("p"))brand="Dare";
+		if(notBrand.contains(brand))
+			return;
+		for(int x=0;x<brands.size();x++) {
+			if(brands.get(x).equals(brand))
+				return;
+		}
+		brands.add(brand);
+	}
+	 public void addBrand() {
+			DesiredCapabilities caps=new DesiredCapabilities();
+			caps.setJavascriptEnabled(true);
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "D:/phantomjs-2.1.1-windows/bin/phantomjs.exe");
+		for(int ca=0;ca<linksBrand.length;ca++){
+			ABdriver=new PhantomJSDriver(caps);
+			String parentUrl=url+linksBrand[ca];
+			ABdriver.get(parentUrl);
+			checkPageIsReady(ABdriver);
+			System.out.println(parentUrl);
+			WebElement rmBrand=ABdriver.findElement(By.id("rm-Brand"));
+			if(hasMoreBrand(rmBrand)) {
+				WebElement a=rmBrand.findElement(By.className("moreLess"));
+				tempDri=new PhantomJSDriver(caps);
+				tempDri.get(url+a.getAttribute("link"));
+				a=tempDri.findElement(By.id("rm-Brand"));
+				List<WebElement> lis=a.findElements(By.tagName("li"));
+				for(int x=0;x<lis.size()-1;x++) {
+					String brand="";
+					brand=lis.get(x).findElement(By.tagName("input")).getAttribute("name");
+					if(brand.contains("n/a")||brand.contains("N/A")) {
+						
+					}
+					else {
+						brand=removeMarkSymbol(brand);
+						addToBrands(brand);
+					}
+				}
+				tempDri.quit();
+			}
+			else {
+				WebElement a=ABdriver.findElement(By.id("rm-Brand"));
+				List<WebElement> lis=a.findElements(By.tagName("li"));
+				for(int x=0;x<lis.size()-1;x++) {
+					String brand="";
+					brand=lis.get(x).findElement(By.tagName("input")).getAttribute("name");
+					if(brand.contains("n/a")||brand.contains("N/A")) {
+						
+					}
+					else {
+						brand=removeMarkSymbol(brand);
+						addToBrands(brand);
+					}
+				}
+			}
+			ABdriver.quit();
+		}
+	 }
+	public void execute(String targetFile)  throws Exception{
+		status=false;
+		addBrand();
+		target=new File(targetFile);
+		fw=new PrintWriter(new FileWriter(target));
+		DesiredCapabilities caps=new DesiredCapabilities();
+		caps.setJavascriptEnabled(true);
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "D:/phantomjs-2.1.1-windows/bin/phantomjs.exe");
+	for(int ca=0;ca<categories.length;ca++){
+		driver=new PhantomJSDriver(caps);
+		String parentUrl=url+links[ca];
+		driver.get(parentUrl);
+		checkPageIsReady(driver);
+		System.out.println(parentUrl);
+		if(ca==0) {
+			fw.println("{\""+categories[ca]+"\":[");
+		}
+		else {
+			fw.println("\""+categories[ca]+"\":[");
+		}
+		fw.flush();
+			WebElement e=driver.findElement(By.className("page-select"));
+			List<WebElement> c=e.findElements(By.tagName("option"));
+			String pre="/page-";
+			for(int x=0;x<c.size();x++) {
+
+
+				dri=new PhantomJSDriver(caps);
+
+				dri.get(parentUrl+pre+(x+1));
+				checkPageIsReady(dri);
+				List<WebElement> products=dri.findElements(By.className("thumb-inner-wrap"));
+				System.out.println("Size of"+dri.getTitle()+" is: "+products.size());
+				int nameNotNullCount=0;
+				int priceNotNullCount=0;
+				ArrayList<String> names=new ArrayList<>();
+				ArrayList<String> prices=new ArrayList<>();
+				ArrayList<String> descriptions=new ArrayList<>();
+				int combo=0;
+				int loopCount=0;
+			do {	
+				combo=0;
+				names=new ArrayList<>();
+				prices=new ArrayList<>();
+				descriptions=new ArrayList<>();
+				int tempCombo=0;
+				boolean lastIsNull=false;
+				for(int y=0;y<products.size();y++) {
+					String name="";
+					String description="";
+					String price="";
+					name=products.get(y).findElement(By.className("details")).findElement(By.className("thumb-header")).getText();
+					description=products.get(y).findElement(By.className("details")).findElement(By.className("description")).getText();
+					name=name.replaceAll("\"", "'");					
+					name=name.replaceAll("\r\n", " ");				
+					name=name.replaceAll("\n", " ");
+					name=name.replaceAll("\r", " ");
+					name=name.replaceAll("Mr .", "Mr.").replaceAll("Mr. ", "Mr.");
+					name=removeMarkSymbol(name);
+					name=replaceUmlaute(name);
+					if(description.length()>40)description="null";//omit useless long text description
+					else {
+						description=description.replaceAll("\"", "'");
+						description="\""+description+"\"";
+					}
+					description=description.replaceAll("\r\n", " ");
+					description=description.replaceAll("\n", " ");
+					description=description.replaceAll("\r", " ");
+					if(ca!=0&&ca!=3) {
+						price=products.get(y).findElement(By.cssSelector("div[class='price-current']")).getText();
+					}
+					else {
+						price=products.get(y).findElement(By.cssSelector("div[class='price-current width-adjusted']")).getText();						
+					}
+					if(price.length()>1) {
+						if(!price.contains("$")) {
+							price=price.substring(0,price.length()-1);
+							Double temp=Double.parseDouble(price);
+							temp=temp/100;
+							price="$"+temp.toString();
+						}
+						price="\""+price+"\"";
+					}
+					else {
+						price="null";
+					}
+					names.add(name);
+					prices.add(price);
+					descriptions.add(description);
+					if(!name.equals("")) {
+						nameNotNullCount++;
+					}
+					if(!price.equals("null")) {
+						priceNotNullCount++;
+						if(tempCombo>0) {
+							if(tempCombo>combo)combo=tempCombo;
+							tempCombo=0;
+						}
+						lastIsNull=false;
+					}
+					else {
+						if(lastIsNull) {
+							tempCombo++;
+							if(tempCombo>combo)combo=tempCombo;
+						}
+						lastIsNull=true;
+					}
+					System.out.println(name+" "+description+" "+price);
+				}
+				loopCount++;
+				if(loopCount>100) {
+					break;
+				}
+			}while((products.size()>0&&(nameNotNullCount==0||priceNotNullCount==0))||combo>3);
+				dri.quit();
+				for(int y=0;y<products.size();y++) {
+					String brand="";
+					brand=getBrand(brands,names.get(y));
+					if(brand==null) {
+						brand="null";
+					}
+					else if(brand.equals("Oreo")) {//special case for  Oreo( it seems should be  Christie) 
+						brand="Christie";
+						if(names.get(y).contains(brand)) {
+							names.set(y, names.get(y).replace(brand+" ", ""));
+						}
+						if(names.get(y).contains("NESTLÉ")) {
+							names.set(y,names.get(y).replace("NESTLÉ", ""));
+						}
+						brand="\""+brand+"\"";
+					}
+					else {
+						//special case for Aquafina 1L  & Evian  & Dasani  &  7UP  & Monster Energy  
+						if(names.get(y).equals("Aquafina")||names.get(y).equals("Evian")||names.get(y).equals("Dasani")||names.get(y).equals("7UP")||names.get(y).equals("Monster Energy")) {
+							if(names.get(y).equals("7UP")) {
+								names.set(y, "7 UP");
+							}
+							else if(names.get(y).equals("Monster Energy")) {
+								names.set(y, "drink");
+							}
+							else {
+								names.set(y,"water");
+							}
+						}
+						names.set(y, removeBrand(names.get(y),brand));
+						brand="\""+brand+"\"";
+					}
+					if(x==c.size()-1&&y==products.size()-1) {
+						fw.println("{\"category\":\""+categories[ca]+"\",\"name\":\""+names.get(y)+"\",\"price\":"+prices.get(y)+",\"description\":"+descriptions.get(y)+",\"brand\":"+brand+",\"store\":\"Walmart\"}");
+					}
+					else {
+						fw.println("{\"category\":\""+categories[ca]+"\",\"name\":\""+names.get(y)+"\",\"price\":"+prices.get(y)+",\"description\":"+descriptions.get(y)+",\"brand\":"+brand+",\"store\":\"Walmart\"},");
+					}
+					fw.flush();					
+				}
+				if(nameNotNullCount==0||priceNotNullCount==0||products.size()==0) {
+					failUrl.add(parentUrl+pre+(x+1));
+				}
+				}
+			//}
+			if(ca==categories.length-1) {
+				fw.println("]}");
+			}
+			else {
+				fw.println("],");
+			}
+			fw.flush();
+			driver.quit();
+			status=true;
+		}
+	}
+	 public void checkPageIsReady(WebDriver driver) {
+		  
+		  JavascriptExecutor js = (JavascriptExecutor)driver;
+		  
+		  
+		  //Initially bellow given if condition will check ready state of page.
+		  if (js.executeScript("return document.readyState").toString().equals("complete")){ 
+		   System.out.println("Page Is loaded.");
+		   return; 
+		  } 
+		  
+		  //This loop will rotate for 40 times to check If page Is ready after every 1 second.
+		  for (int i=0; i<40; i++){ 
+		   try {
+		    Thread.sleep(1000);
+		    }catch (InterruptedException e) {} 
+		   //To check page ready state.
+		   if (js.executeScript("return document.readyState").toString().equals("complete")){ 
+		    break; 
+		   }   
+		  }
+	 }	 		
+	
+	public String removeMarkSymbol(String r) {
+        r = r.replaceAll(  "\u00B0" ,"").replaceAll("\u00a9","").replaceAll(  "\u00AE" ,"").replaceAll( "\u2122" ,"");
+		return r;
+	}
+	public Walmart() {
+		status=false;
+	}
+	public static void main(String[] args) throws Exception{
+		int count=0;
+		Walmart s = null;
+	//	do {
+		try {
+			s=new Walmart();
+			s.execute("D:\\WalmartJsonv3.json");
+		}catch(Exception e) {
+			s.ABdriver.quit();
+			s.tempDri.quit();
+			s.dri.quit();
+			s.driver.quit();
+			System.out.println("check the file path or internet connection and try again later");
+			e.printStackTrace();
+		}
+		finally {
+			System.out.println(count+"                 count             "+count);
+		//	TimeUnit.SECONDS.sleep(40);
+			
+		}
+	//	}while(!Walmart.status);
+		for(int x=0;x<s.failUrl.size();x++) {
+			System.out.println(s.failUrl.get(x));
+		}
+	
+	}
+}
