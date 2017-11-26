33988d883754245515ad241ada4506dbcb632252
ChenWangCarleton <wch4university@gmail.com>
2017-11-26 16:31:15 -0500

    new feature

--- refined version of code/UserSearch.java
+++ refined version of code/UserSearch.java
@@ -3,6 +3,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
@@ -65,7 +67,77 @@ public class UserSearch {
 		 result=result+"]";
 		 return result;//return the result string
 	}
+	public String searchByID(ArrayList<Integer> ids)throws IOException {
+		Collections.sort(ids);
+		int index=0;//index for tracking which id is searching
+		ObjectMapper mapper = new ObjectMapper();
+		File jsonInput=new File(source);
+		InputStream is;
+		is=new FileInputStream(jsonInput);
+		JsonReader reader=Json.createReader(is);
+		JsonObject itObj=reader.readObject();
+		reader.close();		
+		String result="[";
+		outerLoop:
+		for(int x=0;x<categories.length;x++) {
+			ArrayList<mainItem>  Itemss=new ArrayList<>();
 
+			 JsonArray itsObj=itObj.getJsonArray(categories[x]);
+			 for(JsonValue value: itsObj) {
+				 Itemss.add(mapper.readValue(value.toString(), mainItem.class));
+			 }
+			 for(int o=0;o<Itemss.size();o++) {
+				 if(ids.get(index)==Itemss.get(o).productID) {
+					 	index++;
+						String price="[";
+						String store="[";
+						for(int i=0;i<storesOrder.length;i++) {
+							if(Itemss.get(o).stores[i]!=null) {
+								if(i!=storesOrder.length-1) {
+									if(Itemss.get(o).price[i]!=null) {
+										price=price+"\""+Itemss.get(o).price[i]+"\",";
+									}
+									else {
+										price=price+"null,";
+									}
+									store=store+"\""+Itemss.get(o).stores[i]+"\",";
+								}
+								else {
+									if(Itemss.get(o).price[i]!=null) {
+										price=price+"\""+Itemss.get(o).price[i]+"\"";
+									}
+									else {
+										price=price+"null";
+									}
+									store=store+"\""+Itemss.get(o).stores[i]+"\"";							
+								}
+							}
+							else {
+								if(i!=storesOrder.length-1) {
+									price=price+"null,";
+									store=store+"null,";
+								}
+								else {
+									price=price+"null";
+									store=store+"null";							
+								}						
+							}
+						}
+						price+="]";
+						store+="]";
+						String description=Itemss.get(o).description==null?"null":"\""+Itemss.get(o).description+"\"";
+						String brand=Itemss.get(o).brand==null?"null":"\""+Itemss.get(o).brand+"\"";
+						if(index==1)result=result+"{\"productID\":"+Itemss.get(o).productID+",\"category\":\""+Itemss.get(o).category+"\",\"name\":\""+Itemss.get(o).name+"\",\"brand\":"+brand+",\"stores\":"+store+",\"price\":"+price+",\"description\":"+description+"}";
+						else result=result+","+"{\"productID\":"+Itemss.get(o).productID+",\"category\":\""+Itemss.get(o).category+"\",\"name\":\""+Itemss.get(o).name+"\",\"brand\":"+brand+",\"stores\":"+store+",\"price\":"+price+",\"description\":"+description+"}";
+					// return result;
+						///here
+						if(index==ids.size())break outerLoop;
+				 }
+			 }
+		}
+		result=result+"]";
+		return result;
+	}
 	public String searchByID(int id) throws IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		File jsonInput=new File(source);
@@ -138,7 +210,16 @@ public class UserSearch {
 		//us.inilDB("D:\\MainJsonUpdate.json");
 		
 		try {//getall
-			System.out.println(us.generalSearch());
+			
+			ArrayList<Integer> ids=new ArrayList<Integer>();
+			ids.add(100);
+		//	ids.add(5);
+		//	ids.add(1000);
+		//	ids.add(10000);
+		//	ids.add(2000);
+			
+			
+			System.out.println(us.searchByID(ids));
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
