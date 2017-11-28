57b1f193a2f966da2f7074cd518436c16c462811
ChenWangCarleton <wch4university@gmail.com>
2017-11-28 14:26:58 -0500

    grocery server

---
+++ GroceryGoServer/src/GroceryGoServer.java
@@ -0,0 +1,74 @@
+
+//import java.util.Scanner;
+import java.net.*;
+import java.util.ArrayList;
+import java.io.*;
+
+public class GroceryGoServer {
+
+	/* UDP datagram packets and sockets used to send / receive. */
+	private static ServerSocket receiveSocket;
+	// private DatagramSocket sendPacket;
+
+	private int threadNumber;
+	private ClientConnection clientConnection;
+	private String alldata; 
+	/// private boolean running;
+
+	private GroceryGoServer() {
+		
+		
+		System.out.println("Please wait.Loading data...");
+		alldata = "";
+
+		ArrayList<String> source = new ArrayList<>();
+		source.add("src/Loblaws.json");
+		source.add("src/Independent.json");
+		source.add("src/Walmart.json");
+		String target="src/Main.json";
+		UserSearch us=new UserSearch(source,target);
+	
+	try {
+		alldata = us.generalSearch();
+	} catch (IOException e1) {
+		e1.printStackTrace();
+	}
+
+	}
+
+	public void receiveRequest() {
+		System.out.println("Server Started.");
+		threadNumber = 0;
+		try {
+			receiveSocket = new ServerSocket(60800);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		while (true) {
+
+			Socket connectionSocket = null;
+			try {
+				System.out.println("Waiting For Request");
+				connectionSocket = receiveSocket.accept();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+
+			threadNumber++;
+			clientConnection = new ClientConnection(connectionSocket, threadNumber,alldata);
+			clientConnection.start();
+
+
+		}
+	}
+
+
+	public static void main(String args[]) throws IOException {
+		GroceryGoServer server = new GroceryGoServer();
+		server.receiveRequest();
+
+	}
+
+}
